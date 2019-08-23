64df37f072fecb6ca91f298529c5acf1813f6e35
Rene <renecruzatx@outlook.com>
2019-08-23 12:42:53 -0500

    Added functionality for fetching single posts

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -3,20 +3,22 @@ const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      { 
-        _id: '1',
-        title: 'First Post', 
-        content: 'This is the first post!',
-        imageUrl: 'images/books.png',
-        creator: {
-          name: 'Rene Cruz'
-        },
-        createdAt: new Date()
+  Post
+    .find()
+    .then((posts) => {
+      res
+        .status(200)
+        .json({
+          message: 'Fetched posts successfully.', 
+          posts: posts
+        })
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
       }
-    ]
-  });
+      next(err);
+    }); 
 };
 
 exports.createPost = (req, res, next) => {
@@ -51,3 +53,22 @@ exports.createPost = (req, res, next) => {
     });  
 };
  
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post
+    .findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Cannot find post');
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({message: 'Post fetched.', post: post });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
