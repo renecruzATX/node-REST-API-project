d0162562ecfbd031faaa9b9ae80e04e00a50def8
Rene <renecruzatx@outlook.com>
2019-08-23 12:42:53 -0500

    Added file upload capabilities

--- server/app.js
+++ server/app.js
@@ -4,13 +4,42 @@ const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const mongoose = require('mongoose');
+const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + '-' + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
-app.use(bodyParser.json()); // application/json
+app.use(bodyParser.json());
+app.use(
+    multer({
+        storage: fileStorage, 
+        fileFilter: fileFilter
+    })
+    .single('image')
+);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
@@ -23,7 +52,7 @@ app.use((req, res, next) => {
 app.use('/feed', feedRoutes);
 
 app.use((error, req, res, next) => {
-   console.log(err);
+   console.log(error);
    const status = error.statusCode || 500;
    const message = error.message;
    res.status(status).json({message: message}); 
--- server/routes/feed.js
+++ server/routes/feed.js
@@ -6,21 +6,23 @@ const feedController = require('../controllers/feed');
 
 const router = express.Router();
 
+const validate = [
+    body('title')
+      .trim()
+      .isLength({ min: 5 }),
+    body('content')
+      .trim()
+      .isLength({ min: 5 })
+  ];
+
 // GET /feed/posts
 router.get('/posts', feedController.getPosts);
 
 // POST /feed/post
-router.post(
-    '/post', 
-    [
-        body('title')
-            .trim()
-            .isLength({min: 5}),
-        body('content')
-            .trim()
-            .isLength({min: 5})
-    ], 
-    feedController.createPost
-);
+router.post('/post', validate, feedController.createPost);
+
+router.get('/post/:postId', feedController.getPost);
+
+router.put('/post/:postId', validate, feedController.updatePost);
 
 module.exports = router;
