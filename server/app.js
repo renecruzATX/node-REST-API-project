0330f97ba0330b77ef207e085abc423ceb7aee4c
Rene <renecruzatx@outlook.com>
2019-08-26 22:05:27 -0500

    Added signup functionality

--- server/app.js
+++ server/app.js
@@ -5,8 +5,11 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 const mongoose = require('mongoose');
 const multer = require('multer');
+const cors = require('cors');
+
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -42,20 +45,26 @@ app.use(
 );
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+app.use(cors());
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader(
+//       'Access-Control-Allow-Methods',
+//       'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+//     );
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+//   });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
    console.log(error);
    const status = error.statusCode || 500;
    const message = error.message;
-   res.status(status).json({message: message}); 
+   const data = error.data;
+   res.status(status).json({message: message, data: data}); 
 });
 
 mongoose
