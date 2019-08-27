740a4f80f4ef57e9e349c033d075b3981ad7dbfe
Rene <renecruzatx@outlook.com>
2019-08-27 13:12:07 -0500

    Changed getPost function to async await for practice

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -6,36 +6,29 @@ const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 const User = require('../models/user');
 
-exports.getPosts = (req, res, next) => {
+exports.getPosts = async (req, res, next) => {
   const currentPage = req.query.page || 1;
   const perPage = 2;
-  let totalItems;
-  Post
-    .find()
-    .countDocuments()
-    .then(count => {
-      totalItems = count;
-      return Post
-        .find()
-        .skip((currentPage - 1) * perPage)
-        .limit(perPage);       
-    })
-    .then((posts) => {
-      res
-        .status(200)
-        .json({
-          message: 'Fetched posts successfully.', 
-          posts: posts,
-          totalItems: totalItems
-        })
-    })
-    .catch(err => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
-  
+  try {
+    const totalItems = await Post.find().countDocuments()    
+    const posts = await Post
+      .find()
+      .populate('creator')
+      .skip((currentPage - 1) * perPage)
+      .limit(perPage);       
+    res
+      .status(200)
+      .json({
+        message: 'Fetched posts successfully.', 
+        posts: posts,
+        totalItems: totalItems
+      });
+  } catch (err) {
+    if (!err.statusCode) {
+          err.statusCode = 500;
+        }
+        next(err);
+  }  
 };
 
 exports.createPost = (req, res, next) => {
@@ -89,6 +82,7 @@ exports.getPost = (req, res, next) => {
   const postId = req.params.postId;
   Post
     .findById(postId)
+    .populate('creator')
     .then(post => {
       if (!post) {
         const error = new Error('Cannot find post');
